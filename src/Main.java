74ac9427b900695a9119e67ee7fb2be3ce8e1d08
D1288 <107957273+D1288@users.noreply.github.com>
2023-10-26 15:48:58 -0700

    Added functionality for users to choose multiple different sided dice within the same roll

--- src/Main.java
+++ src/Main.java
@@ -6,25 +6,35 @@ public class Main {
         Random randomNum = new Random();
         int totalScore = 0;
         ArrayList<Dice> diceList = new ArrayList<>();
+        
+        int dieSides;
+        int numOfDie;
+        while (true) {
+            System.out.println("Enter the number of sides your dice has");
+            dieSides = scanner.nextInt();
 
-        System.out.println("Enter the number of sides your dice has");
-        int dieSides = scanner.nextInt();
+            System.out.println("How many of these die are you rolling?");
+            numOfDie = scanner.nextInt();
 
-        System.out.println("How many of these die are you rolling?");
-        int numOfDie = scanner.nextInt();
+            for (int i = 0; i < numOfDie; i++) {
+                diceList.add(new Dice(dieSides));
+            }
 
+            System.out.println("Would you like to add more die to the roll? Yes/No");
+            String response = scanner.next();
 
-        for(int i =0; i < numOfDie; i++){
-            diceList.add(new Dice(dieSides));
+            if (response.equalsIgnoreCase("No")) {
+                break;
+            }
         }
 
-        for(Dice dice : diceList){
+        for (Dice dice : diceList) {
             int dieScore = randomNum.nextInt(dice.dieSides) + 1;
-            System.out.println(dieScore);
+            System.out.println("The " + dice.dieSides + " sided die rolled a " + dieScore);
             totalScore += dieScore;
         }
 
-        System.out.println(totalScore);
+        System.out.println("Your total is: " + totalScore);
 
     }
 }

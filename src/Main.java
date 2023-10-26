cfa69dbe038f5f6e2a249cf8fa4b92f36a9d4d88
D1288 <107957273+D1288@users.noreply.github.com>
2023-10-26 15:10:04 -0700

    Added based functionality for rolling individual dice. Users can set the number of sides of their dice and the total number of each die they are rolling

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random randomNum = new Random();
+        int totalScore = 0;
 
+        System.out.println("Enter the number of sides your dice has");
+        int dieSides = scanner.nextInt();
+
+        System.out.println("How many of these die are you rolling?");
+        int numOfDie = scanner.nextInt();
+
+        for(int i = 0; i<= numOfDie -1; i++){
+            int dieScore = randomNum.nextInt(dieSides) + 1;
+            System.out.println(dieScore);
+            totalScore += dieScore;
+        }
+
+        System.out.println(totalScore);
 
     }
 }
